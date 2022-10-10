e9049958fe554a39e6fe55eda29f51f190b000e2
ToniLucG <tgajic@unipu.hr>
2022-10-10 13:09:18 +0200

    zadatak 2

---
+++ zadatak2.js
@@ -0,0 +1,9 @@
+function check(b){
+    if (b>=100 & b<=150000){
+        console.log(b,"je unutar [100,150000]")
+    }
+    else{
+        console.log(b,"nije unutar [10,150000]")
+    }
+}
+check(2500)
