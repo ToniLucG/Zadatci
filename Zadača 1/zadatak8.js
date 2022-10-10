e272adff6164e40436b3350c21820687b3d65c32
ToniLucG <tgajic@unipu.hr>
2022-10-10 14:41:48 +0200

    Zadatak 8

--- Zadača 1/zadatak8.js
+++ Zadača 1/zadatak8.js
@@ -1,2 +1,14 @@
-a = {a:1, b:3, c:5}
-b = {a:3, b:3, c:7}
+a = {a:1, b:3, c:4}
+b = {a:2, b:5, c:7}
+
+function compare(a,b){
+    for (const key of Object.keys(a)) {
+        const contains = Object.hasOwn(b, key);
+        if (!contains) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(compare(a, b));
